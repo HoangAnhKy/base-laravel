798efb773b1b3fe2805b274936cbd1eb7a559faa
HoangAnhKy <kyha-ts@hikesiya.co.jp>
2025-01-08 22:50:53 +0700

    fix sider-bar

--- post-comment/resources/views/Layout/sidebar_left.blade.php
+++ post-comment/resources/views/Layout/sidebar_left.blade.php
@@ -1,7 +1,7 @@
-@can("admin")
-    <div class="card overflow-hidden">
-        <div class="card-body pt-3">
-            <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
+<div class="card overflow-hidden">
+    <div class="card-body pt-3">
+        <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
+            @can("admin")
                 <li class="nav-item">
                     <a class="nav-link  {{ Route::is("admin-dashboard.index") ? "text-white bg-primary rounded" : "" }}"
                        href="{{ route("admin-dashboard.index") }}">
@@ -22,34 +22,25 @@
                        href="{{ route("admin-dashboard.comment") }}">
                         <span>Comment</span></a>
                 </li>
-            </ul>
-        </div>
+            @endcan
+            <li class="nav-item">
+                <a class="nav-link  {{ Route::is("dashboard") ? "text-white bg-primary rounded" : "" }}"
+                   href="{{ route("dashboard") }}">
+                    <span>Home</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link  {{ Route::is("feed") ? "text-white bg-primary rounded" : "" }}"
+                   href="{{ route("feed") }}">
+                    <span>Feed</span></a>
+            </li>
+        </ul>
     </div>
-@endcan
-@cannot("admin")
-    <div class="card overflow-hidden">
-        <div class="card-body pt-3">
-            <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
-                <li class="nav-item">
-                    <a class="nav-link  {{ Route::is("dashboard") ? "text-white bg-primary rounded" : "" }}"
-                       href="{{ route("dashboard") }}">
-                        <span>Home</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link  {{ Route::is("feed") ? "text-white bg-primary rounded" : "" }}"
-                       href="{{ route("feed") }}">
-                        <span>Feed</span></a>
-                </li>
-            </ul>
-        </div>
-        <div class="card-body py-2">
-            <p>Lang: {{ __("idea.test_localization")}}</p>
-            <a class="btn btn-link btn-sm" href="{{ route("language", "en") }}">en </a>
-            <a class="btn btn-link btn-sm" href="{{ route("language", "vn") }}">vn </a>
-        </div>
-        <div class="card-footer text-center py-2">
-            <a class="btn btn-link btn-sm" href="{{ route("profile", auth()->id()) }}">View Profile </a>
-        </div>
+    <div class="card-body py-2">
+        <p>Lang: {{ __("idea.test_localization")}}</p>
+        <a class="btn btn-link btn-sm" href="{{ route("language", "en") }}">en </a>
+        <a class="btn btn-link btn-sm" href="{{ route("language", "vn") }}">vn </a>
     </div>
-
-@endcannot
+    <div class="card-footer text-center py-2">
+        <a class="btn btn-link btn-sm" href="{{ route("profile", auth()->id()) }}">View Profile </a>
+    </div>
+</div>
